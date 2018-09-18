e0c179fbce2b85cb3fba00410eb20446adcc0151
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-18 16:42:23 -0400

    part 1: created Tamagotchi constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,9 +1,26 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
-
+class Tamagotchi {
+    constructor(){
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+    }
+    cry(){
+        this.foodInTummy--
+        console.log("The Tamagotchi is crying!!! WAAAH!!!!!")
+        console.log(`current food in tummy: ${this.foodInTummy}`)
+    }
+}
 
 //create new Tamagotchis
-
+let Agghiitchi = new Tamagotchi()
+let Kughipatchi = new Tamagotchi()
 
 //test out your Tamagotchies below via console.logs
+console.log(Agghiitchi)
+console.log(Kughipatchi)
+
+console.log(Agghiitchi.cry())
+console.log(Kughipatchi.cry())
